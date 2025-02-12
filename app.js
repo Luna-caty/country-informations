6527380570cc9ea0236bca2bafd06b95c3b9c296
Luna <lina.gali@yahoo.com>
2025-02-12 23:46:20 +0100

    adding a styling

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 async function guessCountry() {
-    let errorText = document.getElementById("text");
     try {
     let value = document.getElementById("inputText").value;
     let cName = document.getElementById("countryName");
@@ -7,8 +6,7 @@ async function guessCountry() {
     let cPopulation = document.getElementById("countryPopulation");
     let cLanguage = document.getElementById("countryLanguage");
     let image = document.getElementById("flag");
-    
-
+    let resultDiv =document.getElementById("result")
     const response = await fetch(`https://restcountries.com/v3.1/name/${value}`);
     // console.log(response)
     if (!response.ok) {
@@ -20,10 +18,11 @@ async function guessCountry() {
     cCapital.innerHTML =`Capital : ${data[0].capital}`
     cPopulation.innerHTML = ` Population :${data[0].population}`;
     let languages = Object.values(data[0].languages);
-    // console.log(languages);
     cLanguage.innerHTML = ` Offcial Language :${languages}`;
     image.src=data[0].flags.png
     image.style.display="block"
+    resultDiv.style.backgroundColor = "white"
+    resultDiv.style.boxShadow = "8px 8px 8px rgba(56, 56, 56, 0.5)"
   } catch (error) {
     // errorText.innerHTML='Not found'
     
